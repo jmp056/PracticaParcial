05fdae546e9249a907affaaea39b6b8ffd0b5f1b
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-05 00:38:15 -0400

    Agregando migracion Inicial

---
+++ PracticaParcial/Migrations/20200205043535_Inicial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticaParcial.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    PrestamoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(nullable: true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Telefono = table.Column<string>(nullable: true),
+                    Correo = table.Column<string>(nullable: true),
+                    Capital = table.Column<float>(nullable: false),
+                    Semanas = table.Column<int>(nullable: false),
+                    MontoCuotas = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.PrestamoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+        }
+    }
+}
