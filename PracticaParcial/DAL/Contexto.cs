7f256394503655c3a695f8897e4308bfb69d6405
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-05 00:04:48 -0400

    Creando el contexto

---
+++ PracticaParcial/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticaParcial.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = JMP-PC\SQLEXPRESS; DataBase = PracticaParcial; Trusted_Connection = true");
+        }
+    }
+}
